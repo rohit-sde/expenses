e52ee86ac3a68a4f2d5279c78fe9c07eed865e0a
Rohit Singh <rohitsinghpb4@gmail.com>
2026-02-24 12:36:31 +0530

    styles applied on input compnent

--- components/ManageExpense/Input.tsx
+++ components/ManageExpense/Input.tsx
@@ -1,4 +1,5 @@
-import { Text, TextInput, View } from 'react-native';
+import { GlobalStyles } from '@/constants/styles';
+import { StyleSheet, Text, TextInput, TextStyle, View } from 'react-native';
 
 export default function Input({
   label,
@@ -7,10 +8,37 @@ export default function Input({
   label: string;
   textInputConfig: any;
 }) {
+  const inputStyles: TextStyle[] = [styles.input];
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+    color: GlobalStyles.colors.primary700,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: 'top',
+  },
+});
